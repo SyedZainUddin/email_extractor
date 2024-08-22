b837187213d69caff6f3774e51b90e3bd74e71e6
MHanzala980 <muhammadhanzala980@gmail.com>
2024-08-22 04:04:04 -0700

    updated

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 require("dotenv").config();
 const connection = require("./connection");
 const apiRoutes = require("./route");
-app.set('trust proxy', true)
+// app.set('trust proxy', true)
 
 app.use(apiRoutes);
 
@@ -13,8 +13,12 @@ app.use((err, req, res, next) => {
 });
 
 app.get("/", (req, res) => {
-  const ip = req.socket.remoteAddress
- 
+  const ip =
+    req.headers['x-forwarded-for'] ||
+    req.headers['x-real-ip'] ||
+    req.headers['cg-connecting-ip'] ||
+    req.socket.remoteAddress
+
 
   res.send(`Server is runningIP: ${ip}`);
 });
