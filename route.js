3b3af3c2a588d4ba999d6665a58048ca25f95beb
MHanzala980 <muhammadhanzala980@gmail.com>
2024-08-22 03:43:41 -0700

    updated

--- route.js
+++ route.js
@@ -14,13 +14,13 @@ const axios = require('axios');
 // Replace with your IPAPI access key if you have one
 const IPAPI_URL = 'https://ipapi.co';
 
-
+ 
 
 router.use("/images", async (req, res, next) => {
   const { email, firstName, lastName } = req.query;
   // console.log(email, firstName, lastName, country);
   const ip = req.ip
- 
+  
   try {
     // if (!email || !validator.isEmail(email)) {
     //   return res.status(400).send("Invalid email format");
